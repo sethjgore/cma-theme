fc5ca6af86866beacd83c3b4d994c7885c90f5f2
sethjgore <sethjgore@gmail.com>
2014-09-04 12:41:42 -0500

    udpated pages

--- page-templates/community.php
+++ page-templates/community.php
@@ -21,8 +21,6 @@ get_header(); ?>
 							<h1><?php $title = get_the_title(); if(strlen($title)>29) echo '<span style="font-size:35px;">' . $title . '</span>'; else echo $title; ?></h1>
 							<h2><?php the_field('subtitle')?></h2>
 						</div><!-- /.internal-header -->
-
-            <h1>HELLO DAWG!</h1>
 						<?php
 
 							// Include the page content template.
