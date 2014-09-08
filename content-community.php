2c097a319d2b2ff4823f586c8b192215a760534e
sethjgore <sethjgore@gmail.com>
2014-09-08 09:32:17 -0500

    --amend

--- content-community.php
+++ content-community.php
@@ -28,9 +28,9 @@
 		<?php
 
 			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
+				the_title( '<h1 class="entry-title community-title">', '</h1>' );
 			else :
-				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+				the_title( '<h1 class="entry-title community-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 			endif;
 		?>
 
